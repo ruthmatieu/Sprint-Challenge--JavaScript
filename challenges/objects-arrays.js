8da3e4d982a480df69184c7ba9fea492213c3758
matieuruth <matieu.ruth@gmail.com>
2020-03-22 19:21:38 -0400

    challenge 2 completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -75,7 +75,9 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, log the result. */
-const universities = [];
+const universities = graduates.map((element) => {
+  return element.university;
+});
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -84,7 +86,9 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((element) => {
+  return `${element.first_name} ${element.email}`;
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
